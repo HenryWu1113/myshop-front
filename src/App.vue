ac3ec4714c752da9bb64e07564fafd24e9c66a3e
HenryWu1113 <bug910284@gmail.com>
2022-07-27 23:08:23 +0800

    none

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,19 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
+// import { RouterLink, RouterView } from 'vue-router'
 </script>
 
 <template>
-  <el-container>
-    <h1>123456</h1>
-    <RouterLink to="/">Homee</RouterLink>
-    <RouterLink to="/about">About</RouterLink>
-  </el-container>
-  <Router-view />
+  <div id="app">
+    <el-main>
+      <h1>123456</h1>
+      <el-button type="danger">
+        <RouterLink to="/">Home</RouterLink>
+      </el-button>
+      <el-button type="primary">Primary</el-button>
+
+      <RouterLink to="/about">About</RouterLink>
+      <Router-view />
+    </el-main>
+  </div>
+
 </template>
